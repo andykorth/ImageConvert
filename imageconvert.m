91598e6e3f7a6fecaf094a86c64482fc08f9a3f7
Andy Korth <andy@howlingmoonsoftware.com>
2014-11-17 14:11:25 -0600

    Adjust x and y separate. Fix color channels

--- imageconvert.m
+++ imageconvert.m
@@ -25,49 +25,50 @@
 
 int main(int argc, char *argv[])
 {
-	if(argc != 4){
-		printf("usage: imageconvert <scale> <infile> <outfile>\n");
+	if(argc != 5){
+		printf("usage: imageconvert <sizeX> <sizeY> <infile> <outfile>\n");
 		abort();
 	}
 	
 	NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init]; {
-		NSString* in_path = [NSString stringWithCString:argv[2] encoding:NSUTF8StringEncoding];
+		NSString* in_path = [NSString stringWithCString:argv[3] encoding:NSUTF8StringEncoding];
 		CFURLRef in_url = (CFURLRef)[NSURL fileURLWithPath:in_path];
 		
 		CGImageSourceRef image_source = CGImageSourceCreateWithURL(in_url, NULL);
 		CGImageRef image = CGImageSourceCreateImageAtIndex(image_source, 0, NULL);
 		
-		NSString* out_path = [NSString stringWithCString:argv[3] encoding:NSUTF8StringEncoding];
+		NSString* out_path = [NSString stringWithCString:argv[4] encoding:NSUTF8StringEncoding];
 		CFURLRef out_url = (CFURLRef)[NSURL fileURLWithPath:out_path];
 		CFStringRef out_extension = (CFStringRef)[out_path pathExtension];
 		CFStringRef out_type = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, out_extension, NULL);
 		
 		CGImageRef out_image = image;
 		
-		double scale = 1.0;
-		sscanf(argv[1], "%lf", &scale);
+		double scaleX = 1.0;
+		double scaleY = 1.0;
+		sscanf(argv[1], "%lf", &scaleX);
+		sscanf(argv[2], "%lf", &scaleY);
 		
-		if(scale != 1.0){
-			size_t width = CGImageGetWidth(image)*scale;
-			size_t height = CGImageGetHeight(image)*scale;
-			size_t bpc = 8;
-			size_t bpp = 32;
-			size_t stride = width*((bpp + 7)/8);
-			CGColorSpaceRef colorspace = CGImageGetColorSpace(image);
-			CGBitmapInfo bitmap_info = CGImageGetBitmapInfo(image)&kCGBitmapAlphaInfoMask;
-			
-			if(bitmap_info == kCGImageAlphaNone) bitmap_info = kCGImageAlphaNoneSkipLast;
-			
-			void *buffer = calloc(height, stride);
-			CGContextRef context = CGBitmapContextCreate(buffer, width, height, bpc, stride, colorspace, bitmap_info);
-			
-			CGContextSetInterpolationQuality(context, kCGInterpolationHigh);
-			CGContextDrawImage(context, CGRectMake(0.0, 0.0, width, height), image);
-			
-			CGDataProviderRef dataProvider = CGDataProviderCreateWithData(NULL, buffer, stride*height, NULL);
-			out_image = CGImageCreate(width, height, bpc, bpp, stride, colorspace, bitmap_info, dataProvider, NULL, false, kCGRenderingIntentDefault);
-		}
+		size_t width = (int) scaleX;
+		size_t height = (int) scaleY;
+		size_t bpc = 8;
+		size_t bpp = 32;
+		size_t stride = width*((bpp + 7)/8);
+		CGColorSpaceRef colorspace = CGImageGetColorSpace(image);
+		CGBitmapInfo bitmap_info = CGImageGetBitmapInfo(image)&kCGBitmapAlphaInfoMask;
 		
+		if(bitmap_info == kCGImageAlphaNone) bitmap_info = (CGBitmapInfo) kCGImageAlphaNoneSkipLast;
+		if(bitmap_info == kCGImageAlphaLast) bitmap_info = (CGBitmapInfo) kCGImageAlphaNoneSkipLast;
+		
+		void *buffer = calloc(height, stride);
+		CGContextRef context = CGBitmapContextCreate(buffer, width, height, bpc, stride, colorspace, bitmap_info);
+		
+		CGContextSetInterpolationQuality(context, kCGInterpolationHigh);
+		CGContextDrawImage(context, CGRectMake(0.0, 0.0, width, height), image);
+		
+		CGDataProviderRef dataProvider = CGDataProviderCreateWithData(NULL, buffer, stride*height, NULL);
+		out_image = CGImageCreate(width, height, bpc, bpp, stride, colorspace, bitmap_info, dataProvider, NULL, false, kCGRenderingIntentDefault);
+	
 		CGImageDestinationRef image_destination = CGImageDestinationCreateWithURL(out_url, out_type, 1, NULL);
 		CGImageDestinationAddImage(image_destination, out_image, NULL);
 		CGImageDestinationFinalize(image_destination);
